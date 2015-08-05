516f38e6db02eb0f789d24efca8cd23463182205
Jonathan de Faye Feist <d3faye@gmail.com>
2015-08-05 20:16:11 +0100

    Increment patch number
    1.1.1

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: Steam authentication with Discourse
 # about: Authenticate with Discourse with Steam
-# version: 1.1.0
+# version: 1.1.1
 # author: J. de Faye
 # template author: S. Saffron
 
