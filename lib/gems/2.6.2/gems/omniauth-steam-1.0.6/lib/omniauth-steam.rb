bcd3d85c81686215fd3d062be89a216875d5cd39
Guo Xiang Tan <tgx_world@hotmail.com>
2019-05-13 09:10:45 +0800

    Remove gems commited to repo by mistake take 2.

--- lib/gems/2.6.2/gems/omniauth-steam-1.0.6/lib/omniauth-steam.rb
+++
@@ -1,2 +0,0 @@
-require "omniauth-steam/version"
-require File.expand_path("../omniauth/strategies/steam", __FILE__)
--- lib/gems/2.6.2/gems/omniauth-steam-1.0.6/lib/omniauth-steam/version.rb
+++
@@ -1,5 +0,0 @@
-module OmniAuth
-  module Steam
-    VERSION = "1.0.6"
-  end
-end
