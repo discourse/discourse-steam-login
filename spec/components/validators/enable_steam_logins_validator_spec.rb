cd14d99e00fa988e0b4fee6d499631450bd9ba46
Jarek Radosz <jradosz@gmail.com>
2023-12-01 11:24:15 +0100

    DEV: Update ruby linting (#77)
    * DEV: Update ruby linting
    
    * Use named subject in specs
    
    ---------
    
    Co-authored-by: Loïc Guitaut <loic@discourse.org>

--- spec/components/validators/enable_steam_logins_validator_spec.rb
+++ spec/components/validators/enable_steam_logins_validator_spec.rb
@@ -3,18 +3,18 @@
 require "rails_helper"
 
 RSpec.describe EnableSteamLoginsValidator do
-  subject { described_class.new }
+  subject(:validator) { described_class.new }
 
   describe "#valid_value?" do
     describe "when steam_web_api_key has not been set" do
       it "should return true when value is false" do
-        expect(subject.valid_value?("f")).to eq(true)
+        expect(validator.valid_value?("f")).to eq(true)
       end
 
       it "should return false when value is true" do
-        expect(subject.valid_value?("t")).to eq(false)
+        expect(validator.valid_value?("t")).to eq(false)
 
-        expect(subject.error_message).to eq(
+        expect(validator.error_message).to eq(
           I18n.t("site_settings.errors.steam_web_api_key_is_empty"),
         )
       end
@@ -24,11 +24,11 @@ RSpec.describe EnableSteamLoginsValidator do
       before { SiteSetting.steam_web_api_key = "somekey" }
 
       it "should return true when value is false" do
-        expect(subject.valid_value?("f")).to eq(true)
+        expect(validator.valid_value?("f")).to eq(true)
       end
 
       it "should return true when value is true" do
-        expect(subject.valid_value?("t")).to eq(true)
+        expect(validator.valid_value?("t")).to eq(true)
       end
     end
   end
